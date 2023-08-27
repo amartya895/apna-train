332cb6176e0ea0661c2b051a0defcac0c88981fa
Amartya Sen <amartyasen895@gmail.com>
2023-08-28 01:05:18 +0530

    added destination pic

--- apna-train/src/screens/Home.js
+++ apna-train/src/screens/Home.js
@@ -3,6 +3,15 @@ import "./home.css";
 import bangalore from "../images/bangalore.jpeg";
 import chennai from "../images/chennai.jpeg";
 import delhi from "../images/delhi.jpeg";
+import mumbai from "../images/mumbai.jpeg";
+import ahmedabad from "../images/ahmedabad.png";
+import lucknow from "../images/lucknow.png";
+import patna from "../images/patna.png";
+import kolkata from "../images/kolkata.png";
+import varanasi from "../images/varanasi.png";
+import jaipur from "../images/jaipur.png";
+import agra from "../images/agra.png";
+import kerala from "../images/kerala.png";
 
 function Home() {
   return (
@@ -19,22 +28,70 @@ function Home() {
           </div>
           <div className="row1-des-right">
             <div className="img-div">
-                <h3>Chennai</h3>
-            <img src={chennai} alt="" />
+              <h3>Chennai</h3>
+              <img src={chennai} alt="" />
             </div>
             <div className="img-div">
-                <h3>New Delhi</h3>
-            <img src={delhi} alt="" />
+              <h3>New Delhi</h3>
+              <img src={delhi} alt="" />
             </div>
             <div className="img-div">
-                <h3>Bangalore</h3>
-            <img src={bangalore} alt="" />
+              <h3>Bangalore</h3>
+              <img src={bangalore} alt="" />
             </div>
-            
-           
           </div>
         </div>
-        <div className="row2-des"></div>
+        <div className="row2-des">
+          <div className="row2-des-l">
+            <div className="img-div">
+              <h3>Mumbai</h3>
+              <img src={mumbai} alt="" />
+            </div>
+          </div>
+          <div className="row2-des-r">
+            <div className="cols-row2-r">
+              <div className="img-div">
+                <h3>Ahmedabad</h3>
+                <img src={ahmedabad} alt="" />
+              </div>
+
+              <div className="img-div">
+                <h3>Lucknow</h3>
+                <img src={lucknow} alt="" />
+              </div>
+              <div className="img-div">
+                <h3>Patna</h3>
+                <img src={patna} alt="" />
+              </div>
+            </div>
+            <div className="cols-row2-r">
+              <div className="img-div">
+                <h3>Jaipur</h3>
+                <img src={jaipur} alt="" />
+              </div>
+              <div className="img-div">
+                <h3>Agra</h3>
+                <img src={agra} alt="" />
+              </div>
+              <div className="img-div">
+                <h3>Kolkata</h3>
+                <img src={kolkata} alt="" />
+              </div>
+            </div>
+            <div className="cols-row2-r">
+            <div className="img-div">
+                <h3>Kerala</h3>
+                <img src={kerala} alt="" />
+              </div>
+            <div className="img-div">
+                <h3>Varanasi</h3>
+                <img src={varanasi} alt="" />
+              </div>
+              
+             
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
