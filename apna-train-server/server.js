b2baadb809f6993392ae4e51eaff0858080e20e3
Amartya Sen <amartyasen895@gmail.com>
2024-01-08 02:30:42 +0530

    updated proxy

--- apna-train-server/server.js
+++ apna-train-server/server.js
@@ -2,10 +2,10 @@ import express from "express";
 
 import bookingRoute from "./routes/bookingRoute.js";
 import trainRoute from "./routes/trainRoute.js";
-import userRoute from "./routes/userRoute.js";
-import travellerRoute from "./routes/travellerRoute.js";
+import userRoute from "./routes/userRoute.js"
+import travellerRoute from "./routes/travellerRoute.js"
 import cors from "cors";
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
 // import {restrictToLoggedinUserOnly} from "./middlewares/auth.js"
 
 import dbconfig from "./db.js";
@@ -13,6 +13,8 @@ import dbconfig from "./db.js";
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({extended:false}));
 
 app.use(
   cors({
@@ -24,15 +26,14 @@ app.use(
 );
 
 // app.use(cors());
-app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/booking", bookingRoute);
-app.use("/api/pnr", bookingRoute);
-app.use("/api/train", trainRoute);
-app.use("/api/auth", userRoute);
-app.use("/api/user", travellerRoute);
+app.use("/api/pnr",bookingRoute);
+app.use("/api/train" , trainRoute);
+app.use("/api/auth" , userRoute);
+app.use("/api/user",travellerRoute);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port} `);
