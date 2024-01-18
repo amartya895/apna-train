2bc70479a3d79bd3ba817026d7eea3243cd65365
Amartya Sen <amartyasen895@gmail.com>
2024-01-18 15:11:16 +0530

    updated user route middleware

--- apna-train-server/routes/userRoute.js
+++ apna-train-server/routes/userRoute.js
@@ -3,13 +3,13 @@ import {
   signupController,
   loginController,
 } from "../controllers/authController.js";
-// import {uploadMiddleware} from "../middlewares/upload.js";
+import {uploadMiddleware} from "../middlewares/upload.js";
 
 const router = express.Router();
 
 
 
-router.post("/signup", signupController);
+router.post("/signup",uploadMiddleware, signupController);
 router.post("/login", loginController);
 
 
